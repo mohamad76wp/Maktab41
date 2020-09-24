d7e38f8d7a4f051d828dff9eadb3f41b1113820f
MohamadHosein Mahmodian <mohamad76wp@gmail.com>
2020-09-24 16:57:40 +0330

    Create Basic Methods in Piece.py

---
+++ MohamadHossein_Mahmodian_HW5_Maktab41 (Chess Folder)/Piece.py
@@ -0,0 +1,66 @@
+
+class Pawn:
+    def __init__(self,counter,piece_color,i,j):
+        self.counter = counter
+        self.piece_color = piece_color
+        self.i = i
+        self.j = j
+
+    def get_moving_possibility(self):
+        pass
+
+class Rook:
+    def __init__(self,counter,piece_color,i,j):
+        self.counter = counter
+        self.piece_color = piece_color
+        self.i = i
+        self.j = j
+        
+    def get_moving_possibility(self):
+        pass
+
+
+
+class Knight:
+    def __init__(self,counter,piece_color,i,j):
+        self.counter = counter
+        self.piece_color = piece_color
+        self.i = i
+        self.j = j
+
+    def get_moving_possibility(self):
+        pass
+
+class Bishop:
+    def __init__(self,counter,piece_color,i,j):
+        self.counter = counter
+        self.piece_color = piece_color
+        self.i = i
+        self.j = j
+
+    def get_moving_possibility(self):
+        pass
+
+class Queen:
+    def __init__(self,counter,piece_color,i,j):
+        self.counter = counter
+        self.piece_color = piece_color
+        self.i = i
+        self.j = j
+
+    def get_moving_possibility(self):
+        pass
+
+class King:
+    def __init__(self,counter,piece_color,i,j):
+        self.counter = counter
+        self.piece_color = piece_color
+        self.i = i
+        self.j = j
+
+    def get_moving_possibility(self):
+        pass
+
+
+
+
