d843c2c60b26f4bfd80463500b074d34533ab8b6
MohamadHosein Mahmodian <mohamad76wp@gmail.com>
2020-10-02 01:04:22 +0330

    rook moves

--- MohamadHossein_Mahmodian_HW5_Maktab41 (Chess Folder)/Piece.py
+++ MohamadHossein_Mahmodian_HW5_Maktab41 (Chess Folder)/Piece.py
@@ -1,6 +1,6 @@
 
 class Pawn:
-    def __init__(self,counter,piece_color,i,j):
+    def __init__(self, counter, piece_color, i, j):
         self.counter = counter
         self.piece_color = piece_color
         self.row = i
@@ -9,30 +9,54 @@ class Pawn:
     def get_moving_possibility(self):
 
         if self.row == 1 and self.row <= 7:
-            return f'the {self.piece_color} Pawn is in Home and possible moves : ({self.row + 1},{self.column}) or ({self.row + 2},{self.column})'
+            return f'the {self.piece_color} Pawn is in Home and possible moves : ({self.row + 1},{self.column}) or ' \
+                   f'({self.row + 2},{self.column})'
         elif self.row == 6 and self.row >= 0:
-            return f'the {self.piece_color} Pawn is in Home and possible moves : ({self.row - 1},{self.column}) or ({self.row - 2},{self.column})'
+            return f'the {self.piece_color} Pawn is in Home and possible moves : ({self.row - 1},{self.column}) or ' \
+                   f'({self.row - 2},{self.column})'
         else:
             return f'the {self.piece_color} Pawn is not home and possible moves : ({self.row + 1},{self.column})'
 
 
-
-
 class Rook:
-    def __init__(self,counter,piece_color,i,j):
+    def __init__(self, counter, piece_color, i, j):
         self.counter = counter
         self.piece_color = piece_color
-        self.i = i
-        self.j = j
+        self.row = i
+        self.column = j
         
     def get_moving_possibility(self):
 
-        return f'the {self.piece_color} Rook possible moves : ({self.row + 1},{self.column})'
+        if self.row == 0 and self.column == 0:
+            return f'the {self.piece_color} Rook is in home and possible moves : ({self.row + 1 },{self.column}) or ' \
+                   f'({self.row},{self.column + 1}) '
 
+        elif self.row == 7 and self.column == 7:
+            return f'the {self.piece_color} Rook is in home and possible moves : ({self.row - 1 },{self.column}) or ' \
+                   f'({self.row},{self.column - 1}) '
+
+        else:
+            possessive_value_row = self.row - 7
+            possessive_value_column = self.column - 7
+            print(possessive_value_row)
+            print(possessive_value_column)
+
+            negative_value_row = self.row - 0
+            negative_value_column = self.column - 0
+            print(negative_value_row)
+            print(negative_value_column)
+
+            x = f'the {self.piece_color} Rook is not in home and possible moves : ' \
+                   f'({self.row + possessive_value_row },{self.column}) or' \
+                   f' ({self.row},{self.column + possessive_value_column}) '
+
+            return f'the {self.piece_color} Rook is not in home and possible moves : ' \
+                   f'({self.row + possessive_value_row },{self.column}) or' \
+                   f' ({self.row},{self.column + possessive_value_column}) '
 
 
 class Knight:
-    def __init__(self,counter,piece_color,i,j):
+    def __init__(self, counter, piece_color, i, j):
         self.counter = counter
         self.piece_color = piece_color
         self.i = i
@@ -40,9 +64,10 @@ class Knight:
 
     def get_moving_possibility(self):
         pass
+
 
 class Bishop:
-    def __init__(self,counter,piece_color,i,j):
+    def __init__(self, counter, piece_color, i, j):
         self.counter = counter
         self.piece_color = piece_color
         self.i = i
@@ -50,9 +75,10 @@ class Bishop:
 
     def get_moving_possibility(self):
         pass
+
 
 class Queen:
-    def __init__(self,counter,piece_color,i,j):
+    def __init__(self, counter, piece_color, i, j):
         self.counter = counter
         self.piece_color = piece_color
         self.i = i
@@ -61,8 +87,9 @@ class Queen:
     def get_moving_possibility(self):
         pass
 
+
 class King:
-    def __init__(self,counter,piece_color,i,j):
+    def __init__(self, counter, piece_color, i, j):
         self.counter = counter
         self.piece_color = piece_color
         self.i = i
