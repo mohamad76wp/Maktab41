497717cc728d30bdfb8af4fed1b485b167db0f01
mohamad76wp <mohamad76wp@gmail.com>
2021-02-20 19:22:24 +0330

    change the static directory name

--- django-projects/personal_shop/personal_shop/settings.py
+++ django-projects/personal_shop/personal_shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'orders',
     'products',
+    'siteview',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'personal_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"template")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
