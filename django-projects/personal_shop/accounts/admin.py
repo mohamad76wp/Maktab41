117f1fde4d94b73b43a6f1713c51e5d191b99028
mohamad76wp <mohamad76wp@gmail.com>
2021-02-23 10:57:53 +0330

    Move shop model to products

--- django-projects/personal_shop/accounts/admin.py
+++ django-projects/personal_shop/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Email, Shop, Address
+from .models import User, Email,Address
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.admin import UserAdmin as UserBaseAdmin
 # Register your models here.
@@ -45,3 +45,4 @@ class UserAdmin(UserBaseAdmin):
         EmailAdmin,
         AddressAdmin,
     )
+
