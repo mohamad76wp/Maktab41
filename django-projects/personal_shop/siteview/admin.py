7b1f42d2cd3c8381706ec41114c04fa8ca1fc9e6
mohamad76wp <mohamad76wp@gmail.com>
2021-03-15 21:58:58 +0330

    create carousel modelAdmin,TabularInline

--- django-projects/personal_shop/siteview/admin.py
+++ django-projects/personal_shop/siteview/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Carousel,Carousel_image
 # Register your models here.
+
+
+class  ImageInline(admin.TabularInline):
+    model = Carousel_image
+
+@admin.register(Carousel)
+class CarouselAdmin(admin.ModelAdmin):
+    list_display = ("name", "status")
+    search_fields = ("name",)
+    inlines=[
+        ImageInline
+    ]
