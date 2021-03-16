5354bd65c690f9eaff8c6a6f8872b98b703097b3
mohamad76wp <mohamad76wp@gmail.com>
2021-03-16 11:50:19 +0330

    configure media rood directory

--- django-projects/personal_shop/personal_shop/urls.py
+++ django-projects/personal_shop/personal_shop/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     # path('orders/', include('orders.urls')),
     # path('products/', include('products.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
